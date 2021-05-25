e65b8a4eb7857f7a0792e844dc0175ecb24902d6
Wolfs Kitten <36322994+Dinoboy173@users.noreply.github.com>
2021-05-25 22:27:32 +1000

    fixed the remove unit tests

--- CodeDesignAndDataStructures/AIEUnitTests/AIEUnitTests.cpp
+++ CodeDesignAndDataStructures/AIEUnitTests/AIEUnitTests.cpp
@@ -133,115 +133,97 @@ namespace AIEUnitTests
 			Assert::IsTrue(list.Count() == 0);
 		}
 
-		TEST_METHOD(Remove_From_List)
+		TEST_METHOD(Remove_From_List_Middle)
 		{
-			// Test 1 in middle
+			LinkedList<int> list = { 10, 20, 30, 40 };
 
-			LinkedList<int> list1 = { 10, 20, 30, 40 };
-
-			for (auto iter = list1.begin(); iter != list1.end();)
+			for (auto iter = list.begin(); iter != list.end();)
 			{
+
 				if (*iter == 30)
-				{
-					list1.Remove(iter);
-				}
+					iter = list.Remove(iter);
+				else
+					iter++;
 			}
 
-			Assert::IsNotNull(list1.FirstNode());
-			Assert::IsNotNull(list1.LastNode());
+			Assert::IsNotNull(list.FirstNode());
+			Assert::IsNotNull(list.LastNode());
 
-			Assert::AreEqual(list1.FirstNode()->value, 10);
-			Assert::AreEqual(list1.LastNode()->value, 40);
+			Assert::AreEqual(list.FirstNode()->value, 10);
+			Assert::AreEqual(list.LastNode()->value, 40);
 
-			Assert::IsFalse(list1.IsEmpty());
+			Assert::IsFalse(list.IsEmpty());
 
-			Assert::IsTrue(list1.Count() == 3u);
+			Assert::IsTrue(list.Count() == 3u);
+		}
+		
+		TEST_METHOD(Remove_From_List_End)
+		{
+			LinkedList<int> list = { 10, 20, 30, 40 };
 
-			for (auto iter = list1.begin(); iter != list1.end();)
-			{
-				Assert::IsFalse(*iter == 30);
-			}
-
-			// Test 2 on end
-
-			LinkedList<int> list2 = { 10, 20, 30, 40 };
-
-			for (auto iter = list2.begin(); iter != list2.end();)
+			for (auto iter = list.begin(); iter != list.end();)
 			{
 				if (*iter == 40)
-				{
-					list2.Remove(iter);
-				}
+					iter = list.Remove(iter);
+				else
+					iter++;
 			}
 
-			Assert::IsNotNull(list2.FirstNode());
-			Assert::IsNotNull(list2.LastNode());
+			Assert::IsNotNull(list.FirstNode());
+			Assert::IsNotNull(list.LastNode());
 
-			Assert::AreEqual(list2.FirstNode()->value, 10);
-			Assert::AreEqual(list2.LastNode()->value, 30);
+			Assert::AreEqual(list.FirstNode()->value, 10);
+			Assert::AreEqual(list.LastNode()->value, 30);
 
-			Assert::IsFalse(list2.IsEmpty());
+			Assert::IsFalse(list.IsEmpty());
 
-			Assert::IsTrue(list2.Count() == 3u);
+			Assert::IsTrue(list.Count() == 3u);
+		}
+		
+		TEST_METHOD(Remove_From_List_Start)
+		{
+			LinkedList<int> list = { 10, 20, 30, 40 };
 
-			for (auto iter = list2.begin(); iter != list2.end();)
-			{
-				Assert::IsFalse(*iter == 40);
-			}
-
-			// Test 3 on beginning
-
-			LinkedList<int> list3 = { 10, 20, 30, 40 };
-
-			for (auto iter = list3.begin(); iter != list3.end();)
+			for (auto iter = list.begin(); iter != list.end();)
 			{
 				if (*iter == 10)
-				{
-					list3.Remove(iter);
-				}
+					iter = list.Remove(iter);
+				else
+					iter++;
 			}
 
-			Assert::IsNotNull(list3.FirstNode());
-			Assert::IsNotNull(list3.LastNode());
+			Assert::IsNotNull(list.FirstNode());
+			Assert::IsNotNull(list.LastNode());
 
-			Assert::AreEqual(list3.FirstNode()->value, 20);
-			Assert::AreEqual(list3.LastNode()->value, 40);
+			Assert::AreEqual(list.FirstNode()->value, 20);
+			Assert::AreEqual(list.LastNode()->value, 40);
 
-			Assert::IsFalse(list3.IsEmpty());
+			Assert::IsFalse(list.IsEmpty());
 
-			Assert::IsTrue(list3.Count() == 3u);
+			Assert::IsTrue(list.Count() == 3u);
+		}
+		
+		TEST_METHOD(Remove_From_List_Multiple)
+		{
+			LinkedList<int> list = { 20, 10, 20, 30, 20 };
 
-			for (auto iter = list3.begin(); iter != list3.end();)
-			{
-				Assert::IsFalse(*iter == 10);
-			}
-
-			// Test 4 remove multiple
-
-			LinkedList<int> list4 = { 20, 10, 20, 30, 20 };
-
-			for (auto iter = list4.begin(); iter != list4.end();)
+			for (auto iter = list.begin(); iter != list.end();)
 			{
 				if (*iter == 20)
-				{
-					list4.Remove(iter);
-				}
+					iter = list.Remove(iter);
+				else
+					iter++;
 			}
 
-			Assert::IsNotNull(list4.FirstNode());
-			Assert::IsNotNull(list4.LastNode());
+			Assert::IsNotNull(list.FirstNode());
+			Assert::IsNotNull(list.LastNode());
 
-			Assert::AreEqual(list4.FirstNode()->value, 10);
-			Assert::AreEqual(list4.LastNode()->value, 30);
+			Assert::AreEqual(list.FirstNode()->value, 10);
+			Assert::AreEqual(list.LastNode()->value, 30);
 
-			Assert::IsFalse(list4.IsEmpty());
+			Assert::IsFalse(list.IsEmpty());
 
-			Assert::IsTrue(list4.Count() == 2u);
-
-			for (auto iter = list4.begin(); iter != list4.end();)
-			{
-				Assert::IsFalse(*iter == 20);
-			}
+			Assert::IsTrue(list.Count() == 2u);
 		}
 
 		TEST_METHOD(Insert_Into_List)
